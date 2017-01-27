bc6fb9e566fab7dc64ce8c9c05971c3310fec72c
Mark Brooks <Markabrooks@gmail.com>
2017-01-26 22:03:46 -0700

    Checkpoint 12 Complete Revised

--- scripts/album.js
+++ scripts/album.js
@@ -27,26 +27,10 @@ var albumMarconi = {
          { title: 'Wrong phone number', duration: '2:15'}
      ]
  };
- 
-var albumBeer = {
-     title: 'Stout Month',
-     artist: 'Vine Street Pub',
-     label: 'Boulder Records',
-     year: '1992',
-     albumArtUrl: 'assets/images/album_covers/19.png',
-     songs: [
-         { title: 'XXX Stouts', duration: '4:31' },
-         { title: 'MindBender', duration: '1:21' },
-         { title: 'Majaraja', duration: '7:37'},
-         { title: 'Tweak', duration: '3:24' },
-         { title: 'Mountain Standard', duration: '4:03'}
-     ]
- };
- 
+  
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
       + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
@@ -57,42 +41,39 @@ var createSongRow = function(songNumber, songName, songLength) {
  };
 
  var setCurrentAlbum = function(album) {
-     // #1
+     
      var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
  
-     // #2
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
  
-     // #3
      albumSongList.innerHTML = '';
  
-     // #4
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
 
- // Elements we'll be adding listeners to
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
- 
+
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
-     
      songListContainer.addEventListener('mouseover', function(event) {
-         if(event.target.parentElement.className === 'album-view-song-item'){
-            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         if (event.target.parentElement.className === 'album-view-song-item') {
+             // Change the content from the number to the play button's HTML
+             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
          }
      });
+     
      for (var i = 0; i < songRows.length; i++) {
          songRows[i].addEventListener('mouseleave', function(event) {
              this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
